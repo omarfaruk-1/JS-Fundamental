db9e479a0364cea201ffab90ae637c6ee5990b35
Ismatul Islam Pranto <prantokhaan@gmail.com>
2025-11-22 01:18:04 +0600

    after index.js file in day-2 folder

---
+++ Day-2/index.js
@@ -0,0 +1,23 @@
+console.log("Day-2 Js");
+
+// Variable: variable are used to store data
+
+// variable declaration keyword or variable specifier are var, let and const
+// variable declaration --> specifier variableName = value
+
+
+/* variable declaration rules:
+1. Variable name start with lattes,underscore or dollar symbol
+2. Variable name can not start with number
+3. Variable name can not be reserved keyword
+4. Variable name con not contain any space
+
+*/
+
+
+/*
+-----------------Specifier---------------
+var: Function scope and can be redeclared
+let: Block scope,can be reassigned
+const: Block scope,can not reassigned
+*/
