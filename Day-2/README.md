79ffc9d0acbe582159d78b375ec1dc477d6d46e6
omarfaruk-1 <farukomar.eg@gmail.com>
2025-11-22 22:54:41 +0600

    finished day2

--- Day-2/README.md
+++ Day-2/README.md
@@ -18,3 +18,27 @@
 var: Function scope and can be redeclared
 let: Block scope,can be reassigned
 const: Block scope,can not reassigned    
+
+# Data types
+
+// data types 
+/*
+? Primitive data types: These are basic data types like string,number,boolean,undefine,null,bigint,symbol
+
+string: text value ("hello")
+number: numeric value (20,3.4-> floating value)
+boolean: True/False value(true,false)
+undefine: variable value not assign (let x;)
+null: variable value nothing (let x=null means x value is nothing)
+big int: bigger number(929832873298432)
+symbol: unique identifiers (Symbol('"id"))
+
+
+? Non-Primitive data type: complex data type these are objects, array and function
+
+object: collection of key value pairs
+array: ordered list of value
+function: code that can be execute
+
+
+*/
--- Day-2/index.js
+++ Day-2/index.js
@@ -21,3 +21,58 @@ var: Function scope and can be redeclared
 let: Block scope,can be reassigned
 const: Block scope,can not reassigned    
 */
+
+
+// var variable declared 
+// var name="Omar Faruk";
+// console.log(name);
+var name ="Faruk";
+console.log(name);
+
+
+//let variables
+let num=20;
+console.log(num);
+
+// let num=10;// can not redeclared block scope
+let fruit;
+fruit="Apple";
+console.log(fruit);
+
+
+// const variable
+// const age; ?we need to give value of variable when we declared a variable 
+const age=20;
+console.log(age);
+
+
+// data types 
+/*
+? Primitive data types: These are basic data types like string,number,boolean,undefine,null,bigint,symbol
+
+string: text value ("hello")
+number: numeric value (20,3.4-> floating value)
+boolean: True/False value(true,false)
+undefine: variable value not assign (let x;)
+null: variable value nothing (let x=null means x value is nothing)
+big int: bigger number(929832873298432)
+symbol: unique identifiers (Symbol('"id"))
+
+
+? Non-Primitive data type: complex data type these are objects, array and function
+
+object: collection of key value pairs
+array: ordered list of value
+function: code that can be execute
+
+
+*/
+
+
+let student={
+    name:"faruk",
+    age:20,
+    location:"Kanchan"
+}
+
+console.log(student.age);
