566c78822f34fa5390253766c71250dac935b921
omarfaruk-1 <farukomar.eg@gmail.com>
2025-12-03 22:10:13 +0600

    Array basic

---
+++ Day-15/index.js
@@ -0,0 +1,10 @@
+const arr=["omar",23,423,false];
+console.log(arr.length);
+console.log(arr[2]);
+arr.push(60)
+console.log(arr)
+
+// array creation 
+
+const array = new Array(3,4)
+console.log(array);
